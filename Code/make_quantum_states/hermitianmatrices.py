283a9fdb2b10ff26782ae79b3496c26922ebd95b
Mingde Yin <mingdeyin@gmail.com>
2021-05-24 23:50:33 -0400

    Adjusted hermitian matrix generation algorithm

--- Code/make_quantum_states/hermitianmatrices.py
+++ Code/make_quantum_states/hermitianmatrices.py
@@ -6,59 +6,14 @@ March 06:
 - some testing functions
 May 04:
 - Plans to add better random matrix generation using U+ H U method
+May 24:
+- New Generators implemented for random unitary operators
 '''
 
 import numpy as np
 from utils import print_matrix
 
 np.random.seed(2)
-
-
-def random_density_matrix(N):
-    '''
-    Makes a random density matrix for an N level system.
-    Broken right now.
-
-    Obeys:
-    - Hermiticity
-    - Tr = 1
-    '''
-    # Off-diagonal elements
-    reals = np.random.random((N, N))
-    imgs = np.random.random((N, N)) * 1j
-
-    # Make the upper trianglular portion of the matrix
-    U = np.triu(reals + imgs, 1)
-
-    # Diagonal elements
-    weights = np.random.random(N)
-    weights = weights / sum(weights)  # normalize trace
-    diagonal = np.diag(weights)
-
-    # Mirror elements over
-    return np.matrix(diagonal + (U.conj().T + U))
-
-
-def random_pure_density_matrix(N):
-    '''
-    Makes a random pure density matrix for an N level system.
-    This is done by taking the state vectors for a pure state.
-
-    Obeys:
-    - Hermiticity
-    - Tr = 1
-    - P^2 = P
-    '''
-    # state vector
-    reals = np.random.random(N)
-    imgs = np.random.random(N) * 1j
-    state = np.matrix((reals+imgs).reshape((N, 1)))
-
-    # normalize
-    state = state / np.linalg.norm(state)
-
-    # make density matrix
-    return np.matmul(state, state.H)
 
 
 def bipartite_state(A, B):
@@ -69,22 +24,42 @@ def bipartite_state(A, B):
     return np.matrix(np.kron(A, B))
 
 
+def rand_diag(N):
+    '''
+    N Dimensional random trace-1 diagonal matrix via the following process:
+        - Generate a random row vector
+        - Divide by the sum to get trace-1
+        - Return diagonal matrix
+    '''
+    diag_elements = np.random.random(N)
+    return np.matrix(np.diag(diag_elements/sum(diag_elements)))
+
+
+def rand_unitary(N):
+    '''
+    Random complex unitary matrix via the following process:
+        - Generate a random complex matrix
+        - Apply QR decomposition to get a unitary Q matrix
+        - Return Q
+    '''
+    reals = np.random.random((N, N))
+    imags = np.random.random((N, N)) * 1j
+
+    a = reals + imags
+    # Run QR decomposition to get unitary operator
+    q, _ = np.linalg.qr(a)
+    return np.matrix(q)
+
+
+def rand_hermitian(N):
+    '''
+    Random trace-1 hermitian matrix of dimension N
+    '''
+    U = rand_unitary(N)
+    H = rand_diag(N)
+
+    return U * H * U.H
+
+
 if __name__ == "__main__":
-    P1 = random_density_matrix(2)
-
-    print("Mixed Density Matrix")
-    print_matrix(P1)
-    print_matrix(P1**2)
-    # Is there anything special we need for the general density matrix?
-    # Do I need to control anything on the off-diagonals?
-
-    P2 = random_pure_density_matrix(2)
-    print("\nPure Density Matrix")
-    print_matrix(P2)
-    print_matrix(P2**2)
-
-    print("\nBig System")
-    P3 = bipartite_state(
-                random_pure_density_matrix(2),
-                random_pure_density_matrix(2))
-    print_matrix(P3)
+    print_matrix(rand_hermitian(3).H)
--- Notes/GeneratingMatrices.md
+++ Notes/GeneratingMatrices.md
@@ -20,3 +20,21 @@ All I have figured out on this front is that the matrix has to be trace 1 and He
 * Generate a random unitary matrix (i.e. Hermitian, orthonormal) (S)
 * H = S(dagger)DS
 This approach is really nice because unitary matrices take care of almost everything for you!!
+
+### Medium Level Implementation
+The above game plan is nice, but let's bring it down into code. 
+
+The basic operation to generate the hermitian matrix is is `M = U * H * U.H`, wherein `U` is a unitary matrix, `H` is a trace-1 diagonal matrix, and `.H` signifies the dagger operator in numpy.
+
+### Generating the diagonal matrix:
+* Generate `N` random real numbers from some probability distribution (Gaussian, blah blah etc.) and divide all by their sum.
+* For this case, we use `np.random.random()`
+* Put these values into a diagonal matrix using `np.diag`, resulting in a trace 1 real diagonal matrix
+
+### Generating the unitary matrix:
+* [Credits to this link](https://mathoverflow.net/questions/333187/random-unitary-matrices) for the rough implementation detail.
+* Generate two sets `N**2` random numbers from some probability distribution, and multiply the second one by `1j` (imaginary unit)
+* Add these together to get a matrix, `A`, which is a complex matrix filled with random values
+* Run a QR decomposition, and get the `Q`, matrix, which is now a unitary operator, with orthonormal eigenvectors similar to the results of a Gram-Schmidt orthonormalization process
+
+Finally, we put this all together. This is demonstrated in the `hermitianmatrices.py` source file.
